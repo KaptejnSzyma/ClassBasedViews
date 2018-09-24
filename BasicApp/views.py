5be35b3dbaf252db6f08890d8a7ae89e73b89a5d
Szymek <szymon.rzadca@gmail.com>
2018-09-24 17:59:17 +0200

    template view example

--- BasicApp/views.py
+++ BasicApp/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
-# Create your views here.
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class Based Views are cool!")
+class IndexView(TemplateView):
+    template_name = 'index.html'
