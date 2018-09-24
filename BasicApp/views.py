c65db63da20045cd64f8aa38507886d527118f7a
Szymek <szymon.rzadca@gmail.com>
2018-09-24 18:05:02 +0200

    injecting data in class based view

--- BasicApp/views.py
+++ BasicApp/views.py
@@ -3,3 +3,8 @@ from django.views.generic import View, TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION'
+        return context
